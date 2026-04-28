9ab04b580682945ac162ef246f572f004d0a313f
sohagroy7223 <freelancersohagroy@gmail.com>
2026-04-28 11:12:10 +0600

    adddad server with mongodb

---
+++ practice-crud-server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express());
+
+// sohagroy7223_db_user
+// nZ1SM80jGVeFAiaS
+const uri =
+  "mongodb+srv://sohagroy7223_db_user:nZ1SM80jGVeFAiaS@crud-practice-cluster.l3ixzxm.mongodb.net/?appName=crud-practice-cluster";
+
+app.get("/", (req, res) => {
+  res.send("practice crud server running");
+});
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`practice app listening on port ${port}`);
+});
